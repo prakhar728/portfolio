4986cac4ca4e14837f105c96b45f85a21c480289
Prakhar Ojha <prakharojha12@gmail.com>
2021-09-21 01:12:05 +0530

    project Section update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,11 +7,27 @@ const Projects = () => {
             <div className={styles.header}>
                 <h1 className={styles.title}>PROJECTS</h1>
             </div>
-            <div className={styles.projectCards}>
-                <h3> AUTHENTICATION TEMPLATE </h3>
-                <img src={proj1} alt="Project1" />
-                <div className={styles.projectInfo}>
-                    This project was made so that it can be cloned and used at it is without worrying about adding external auth.
+            <div className={styles.projectCardContainer}>
+                <div className={styles.projectCards}>
+                    <img src={proj1} alt="Project1" />
+                    <h3> AUTHENTICATION TEMPLATE </h3>
+                    <div className={styles.projectInfo}>
+                        This project was made so that it can be cloned and used at it is without worrying about adding external auth.
+                    </div>
+                </div>
+                <div className={styles.projectCards}>
+                    <img src={proj1} alt="Project1" />
+                    <h3> AUTHENTICATION TEMPLATE </h3>
+                    <div className={styles.projectInfo}>
+                        This project was made so that it can be cloned and used at it is without worrying about adding external auth.
+                    </div>
+                </div>
+                <div className={styles.projectCards}>
+                    <img src={proj1} alt="Project1" />
+                    <h3> AUTHENTICATION TEMPLATE </h3>
+                    <div className={styles.projectInfo}>
+                        This project was made so that it can be cloned and used at it is without worrying about adding external auth.
+                    </div>
                 </div>
             </div>
         </div>
