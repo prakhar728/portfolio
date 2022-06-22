167f740118805a865eeda2a4ed3ae2265951a9a2
Prakhar Ojha <prakhar@getStan.app>
2022-06-22 16:25:05 +0530

    Nav added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded'
 import MenuIcon from '@material-ui/icons/Menu'
 import CloseIcon from '@material-ui/icons/Close'
 import { ThemeContext } from '../../contexts/theme'
-import { projects, skills, contact } from '../../portfolio'
+import { projects, skills, contact,Experiences } from '../../portfolio'
 import './Navbar.css'
 
 const Navbar = () => {
@@ -39,6 +39,18 @@ const Navbar = () => {
               className='link link--nav'
             >
               SKILLS
+            </a>
+          </li>
+        ) : null}
+
+        {Experiences.length ? (
+          <li className='nav__list-item'>
+            <a
+              href='#experience'
+              onClick={toggleNavList}
+              className='link link--nav'
+            >
+              EXPERIENCE
             </a>
           </li>
         ) : null}
